d2f067b6d7525ab096d4aa8270f26819444166c7
kishore kumar reddy nerkundram gajendran <kn272@afsconnect1.njit.edu>
2017-10-04 19:16:42 -0400

    now its 100%

--- index.php
+++ index.php
@@ -8,19 +8,23 @@
   $year = array("2012", "396", "300","2000", "1100", "1089");
   echo "The value of \$year: ";
   print_r($year);
+  echo "<br><br>";
 
   echo "<br>Replacing '-' with '/': ".strtr($date,"-","/")."<br>";
   $date = strtr($date,"-","/");
+  echo "<br><br>";
+
   echo "The if elseif statement execution:<br>";
-  if (strcmp($date>$tar)>0) {
+  if (strcmp($date,$tar)>0) {
      echo "The future<br>";
   }
-  elseif (strcmp($date>$tar)<0) {
+  elseif (strcmp($date,$tar)<0) {
      echo "The past<br>";
   }
-  elseif (strcmp($date>$tar)==0) {
+  elseif (strcmp($date,$tar)==0) {
      echo "Oops<br>";
   }
+  echo "<br><br>";
 
  
   echo "The value of \$date: ".$date."<br>";
@@ -30,29 +34,35 @@
      $pos = strpos($date,"/",$pos+1);
      echo " ".$pos." ";
   }
+  echo "<br><br>";
 
   echo "<br>No. of words in ".$date." is: ".str_word_count($date,0)."<br>";
+  echo "<br><br>";
 
   $sampleText = "This is the sample text for strlen<br>";
 
   echo "$sampleText";
-  echo "The length of sampletext is".strlen($sampleText)."<br>";
-  
+  echo "The length of sampletext is ".strlen($sampleText)."<br>";
+  echo "<br><br>";
+
   $ascii = ord($sampleText);
   echo "The ascii value of 1st charecter of sampleText is: ".$ascii."<br>";
+  echo "<br><br>";
 
   echo "last 2 charecters of DATE are: ".substr($date,-2)."<br>";
-  
+  echo "<br><br>";
+
   echo "After breaking DATE into an array using '/' as
   seperator: <br>";
   print_r(explode("/",$date));
-  echo "<br>";
+  echo "<br><br>";
 
+  echo "leap year?<br>";
   foreach ($year as $value) {
       switch(($value%4!=0)||(($value%100==0)&&($value%400!=0))) {
-         case 1:echo "$value is not a leap year  ";
+         case 1:echo "$value FALSE  ";
 	        break;
-	 case 0:echo "$value is a leap year  ";
+	 case 0:echo "$value TRUE  ";
 	        break;
       }
   }
